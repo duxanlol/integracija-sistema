5299b00975c9942ff85cef4dc9c63ffda173810e
Dušan Mandić <theduxan@gmail.com>
2022-05-31 23:39:53 +0200

    Update CensorDOM.java

--- src/chat/server/CensorDOM.java
+++ src/chat/server/CensorDOM.java
@@ -21,105 +21,105 @@ import org.w3c.dom.ls.LSOutput;
 import org.w3c.dom.ls.LSSerializer;
 import org.xml.sax.SAXException;
 public class CensorDOM {
-	
+
 	public static String xmlPath = "C:\\xml\\reci.xml";
 	private Document dom;
 	private Set<String> censoredWords;
-	
+
 	public CensorDOM() {
 		dom = loadDocument();
 		censoredWords = new HashSet<String>();
 		parseWordsFromXML();
 
 	}
-	
+
 	public Document loadDocument() {
-        // Get a factory
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        try {
-            // Get a new instance of document builder
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            // Parse the file and return DOM representation
-            return builder.parse(xmlPath);
-        // Print any errors
-        } catch (ParserConfigurationException ex) {
-            ex.printStackTrace();
-            return null;
-        } catch (SAXException ex) {
-            ex.printStackTrace();
-            return null;
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-    }
-	
+		// Get a factory
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		try {
+			// Get a new instance of document builder
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			// Parse the file and return DOM representation
+			return builder.parse(xmlPath);
+			// Print any errors
+		} catch (ParserConfigurationException ex) {
+			ex.printStackTrace();
+			return null;
+		} catch (SAXException ex) {
+			ex.printStackTrace();
+			return null;
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+
 	public synchronized void addRec(String rec, String nadimak) {
-        Element root = dom.getDocumentElement();
-        Element recElement = dom.createElement("rec");
-        recElement.setAttribute("nadimak", nadimak);
-        recElement.appendChild(dom.createTextNode(rec.toLowerCase()));
-        root.appendChild(recElement);
-        censoredWords.add(rec);
-        saveDocument();
+		Element root = dom.getDocumentElement();
+		Element recElement = dom.createElement("rec");
+		recElement.setAttribute("nadimak", nadimak);
+		recElement.appendChild(dom.createTextNode(rec.toLowerCase()));
+		root.appendChild(recElement);
+		censoredWords.add(rec);
+		saveDocument();
 	}
-	
+
 	public synchronized boolean removeRec(String rec, String nadimak) {
-        Element root = dom.getDocumentElement();
-        NodeList nl = root.getElementsByTagName("rec");
-        for (int i = 0; i < nl.getLength(); i++) {
-        	if (((Element) nl.item(i)).getAttribute("nadimak").equalsIgnoreCase(nadimak) &&
-        			((Element) nl.item(i)).getTextContent().equalsIgnoreCase(rec)) {
-        		root.removeChild(((Element) nl.item(i)));
-        		censoredWords.remove(rec);
-        		saveDocument();
-        		return true;
-        	}
-        }
-        //Ne cuvamo dokument kad nema promene.
-        return false;
+		Element root = dom.getDocumentElement();
+		NodeList nl = root.getElementsByTagName("rec");
+		for (int i = 0; i < nl.getLength(); i++) {
+			if (((Element) nl.item(i)).getAttribute("nadimak").equalsIgnoreCase(nadimak) &&
+					((Element) nl.item(i)).getTextContent().equalsIgnoreCase(rec)) {
+				root.removeChild(((Element) nl.item(i)));
+				censoredWords.remove(rec);
+				saveDocument();
+				return true;
+			}
+		}
+		//Ne cuvamo dokument kad nema promene.
+		return false;
 	}
-	
+
 	public Set<String> getWords(){
 		return censoredWords;
 	}
-	
+
 	private void parseWordsFromXML() {
 		Element root = dom.getDocumentElement();
-	    NodeList nl = root.getElementsByTagName("rec");
-	    for (int i = 0; i < nl.getLength(); i++) {
-	    	censoredWords.add(((Element) nl.item(i)).getTextContent().toLowerCase());
-	    }
+		NodeList nl = root.getElementsByTagName("rec");
+		for (int i = 0; i < nl.getLength(); i++) {
+			censoredWords.add(((Element) nl.item(i)).getTextContent().toLowerCase());
+		}
 	}
-	
-    private void saveDocument() { 
-        try {
 
-            // Get a registry
-            DOMImplementationRegistry registry = DOMImplementationRegistry.newInstance();
+	private void saveDocument() { 
+		try {
 
-            // Get a new instance of loader/saver
-            DOMImplementationLS impl = (DOMImplementationLS) registry.getDOMImplementation("LS");
-            LSSerializer writer = impl.createLSSerializer();
+			// Get a registry
+			DOMImplementationRegistry registry = DOMImplementationRegistry.newInstance();
 
-            // Write the whole document
-            String text = writer.writeToString(dom);;
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(xmlPath), StandardCharsets.UTF_16BE);
-            outputStreamWriter.write(text);
-            outputStreamWriter.close();
-        // Print any errors
-        } catch (ClassCastException ex) {
-            ex.printStackTrace();
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        } catch (InstantiationException ex) {
-            ex.printStackTrace();
-        } catch (IllegalAccessException ex) {
-            ex.printStackTrace();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+			// Get a new instance of loader/saver
+			DOMImplementationLS impl = (DOMImplementationLS) registry.getDOMImplementation("LS");
+			LSSerializer writer = impl.createLSSerializer();
 
-    }
-	
+			// Write the whole document
+			String text = writer.writeToString(dom);;
+			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(xmlPath), StandardCharsets.UTF_16BE);
+			outputStreamWriter.write(text);
+			outputStreamWriter.close();
+			// Print any errors
+		} catch (ClassCastException ex) {
+			ex.printStackTrace();
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		} catch (InstantiationException ex) {
+			ex.printStackTrace();
+		} catch (IllegalAccessException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
 }
